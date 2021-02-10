f9f3051d769a834a4afcdd4e0943ff61aa4fd3ce
C3NZ <cenz@cenz.io>
2021-02-10 14:25:12 -0800

    [add] all of the logging examples.

--- examples/logging.sh
+++ examples/logging.sh
@@ -4,5 +4,9 @@ pushd "$ROOT_DIR"
 source "$ROOT_DIR/lambda.sh"
 
 LAMBDA_LOG_INFO "This is an info message."
+LAMBDA_LOG_WARN "This is a warning."
+LAMBDA_LOG_TRACE "This is a trace."
+LAMBDA_LOG_ERROR "This is an error."
+LAMBDA_LOG_FATAL "This is a fatal message."
 
 popd  # ROOT_DIR
